1e713807b42a7bec53a5f266b7f82261d830e8ba
teason <akateason@hotmail.com>
2016-04-25 18:27:23 +0800

    Initial Commit

---
+++ EvernoteEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EvernoteEffect
+//
+//  Created by teason on 16/4/25.
+//  Copyright © 2016年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EvernoteEffect/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EvernoteEffect
+//
+//  Created by teason on 16/4/25.
+//  Copyright © 2016年 teason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
